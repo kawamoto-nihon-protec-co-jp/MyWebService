1d752fe34321267efde14d44d369ad0f02ca19fc
kawamoto-nihon-protec-co-jp <kawamoto@nihon-protec.co.jp>
2014-10-18 16:29:10 +0900

    cmt

--- src/main/java/ngdemo/rest/ProductRestService.java
+++ src/main/java/ngdemo/rest/ProductRestService.java
@@ -1,11 +1,14 @@
 package ngdemo.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import ngdemo.bean.TransData;
 import ngdemo.service.IProductService;
 
 @Path("/products")
@@ -26,6 +29,16 @@ public final class ProductRestService {
         String ret = "[{\"name\":\"kawamoto1\"},{\"name\":\"product3\"}]";
 //        return this.service.getProducts();
         return ret;
+    }
+
+    @POST
+    @Path("/putMessage")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public TransData getCluster(TransData data) {
+        TransData responseData = new TransData();
+        responseData.setData(data.getData());
+        return responseData;
     }
 
 }
