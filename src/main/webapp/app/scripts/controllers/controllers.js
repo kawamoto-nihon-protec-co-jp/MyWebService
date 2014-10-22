b8390d178a93ff4f7ebb38b0c7832e56fc9b2a20
kawamoto-nihon-protec-co-jp <kawamoto@nihon-protec.co.jp>
2014-10-22 20:43:50 +0900

    cmt

--- src/main/webapp/app/scripts/controllers/controllers.js
+++
@@ -1,7 +0,0 @@
-var ctrlModule = angular.module("controllers", []);
-
-ctrlModule.controller("ProductCtrl", function($scope, $http) {
-	$http.get("/testapp/api/products").success(function(products) {
-		$scope.products = products;
-	});
-});
--- src/main/webapp/app/test/e2e/test.js
+++
@@ -1,8 +0,0 @@
-describe("Myapp", function() {
-	it("should show 2 products", function() {
-	    browser.get('http://localhost:8080/testapp/app/index.html');
-
-	    expect(browser.getTitle()).toEqual('jersey-angular-test');
-	    expect(element.all(by.repeater('product in products')).count()).toEqual(2);
-	});
-});
